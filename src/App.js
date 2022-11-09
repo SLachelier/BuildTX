06b271950119c51c8707bbad15c005d338673682
garcia2697 <luisgarcia.lg37@gmail.com>
2022-11-09 00:08:03 -0600

    adds sass starter files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
-
 import Navbar from './Components/Navbar';
+import MainPage from './Components/MainPage'
+
 
 function App() {
   return (
     <div className="App">
      <Navbar></Navbar>
+     <MainPage></MainPage>
     </div>
   );
 }
---
+++ src/Components/MainPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../Styles/MainPage.css'
+
+function MainPage(){
+    return (
+        <>
+        <div className="mainPage">
+        <h1> Welcome to the Website</h1>
+        <button> Click Here </button>
+        </div>
+        
+        
+        </>
+    )
+}
+
+export default MainPage;
