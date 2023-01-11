47e803a4396e69aacf971efe9b98966f03d086e3
Luis Garcia <Luis@Luis-MacBook-Pro.local>
2023-01-10 18:36:35 -0600

    adds sidebar feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import Navbar from './Components/Navbar';
+// import Navbar from './Components/Navbar';
 
 // import fetchRoad from './Components/fetchRoad';
 
 // import FetchJob from './Components/FetchJob';
-import Months from './Components/Months';
-import FetchCounty from './Components/FetchCounty';
+// import Months from './Components/Months';
+// import FetchCounty from './Components/FetchCounty';
 // import FetchDistrict from './Components/FetchDistrict';
 // import Test from './Components/test';
 
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 function App() {
   
   
   return (
     <div className="App">
-     <Navbar></Navbar>
      
+     <h1>Ayooo </h1>
+     <Router>
+      <Navbar></Navbar>
+      <Switch>
+        <Route path='/'/>
+      </Switch>
+     </Router>
      
-     <Months></Months>
-     <FetchCounty></FetchCounty>
-     {/* <DataFetching></DataFetching> */}
-     {/* <Test></Test> */}
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+    Outlet,
+    createRoutesFromElements,
+  } from "react-router-dom";
+import Products from "./Routes/Products";
+import Home from "./Routes/Home";
+import Reports from "./Routes/Reports";
+import Navbar from "./Components/Navbar";
+import FetchJob from "./Components/FetchJob";
+import FetchDistrict from "./Components/FetchDistrict";
+import FetchCounty from "./Components/FetchCounty";
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const AppLayout=()=>(
+    <>
+        
+        <Navbar/>
+        <Outlet/>
+    </>
+)
+    
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<AppLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/reports" element={<Reports />} />
+      <Route path="/fetchJobs" element={<FetchJob />} />
+      <Route path="/fetchDistrict" element={<FetchDistrict />} />
+      <Route path="/fetchCounty" element={<FetchCounty />} />
+    </Route>
+  )
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+// const router = createBrowserRouter([
+//     {
+//         element:<AppLayout/>,
+//         children:[
+//             { 
+//                 path:"/",
+//                 element:<Home/>,
+//               },
+//               {
+//                 path:"products",
+//                 element:<Products/>
+//               },
+//               {
+//                 path:"reports",
+//                 element:<Reports/>
+//               },
+//         ]
+//     },
+  
+  
+// ]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
